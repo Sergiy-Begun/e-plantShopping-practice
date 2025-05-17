1590b770fa4d26316f26b90a303eccd1727fc0f1
Sergiy Begun <sergiy.begun@gmail.com>
2025-05-17 12:46:16 +0300

    first commit

---
+++ src/CartSlice.jsx
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+        console.log("before");
+        console.log(state.items[0]);
+        const { name, image, cost } = action.payload; // Destructure product details from the action payload
+        // Check if the item already exists in the cart by comparing names
+        const existingItem = state.items.find(item => item.name === name);
+        
+        if (existingItem) {
+            // If item already exists in the cart, increase its quantity
+            existingItem.quantity++;
+            console.log("inside");
+            console.log(existingItem);
+        } else {
+            // If item does not exist, add it to the cart with quantity 1
+            state.items.push({ name, image, cost, quantity: 1 });
+            console.log("inside1");
+            console.log(state.items);
+        }
+    
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
+
+    
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
